aaff6127b621c78ff740e8599df4f73c506c8de1
corrado <pizzolittocorradoantonio@gmail.com>
2025-09-17 16:03:48 +0200

    italian date fix

--- jobs/utils/italian_time.py
+++ jobs/utils/italian_time.py
@@ -37,8 +37,22 @@ def get_italian_time(only_date_string: bool = False, full_date_format: bool = Fa
         # Format with milliseconds
         return italian_time.strftime('%d/%m/%Y_%H:%M.%f')[:-3]  # Remove last 3 digits to get milliseconds
 
-    # For MongoDB, convert to UTC while preserving the actual time
-    # MongoDB stores dates as UTC, so we need to return a UTC datetime
-    utc_time = italian_time.astimezone(pytz.UTC)
+    # For MongoDB, we create a UTC datetime with Italian time values
+    # This replicates the TypeScript behavior of creating a Date.UTC with Italian time components
+    # Note: This creates a datetime that appears to be Italian time but is marked as UTC
+    year = italian_time.year
+    month = italian_time.month
+    day = italian_time.day
+    hour = italian_time.hour
+    minute = italian_time.minute
+    second = italian_time.second
+    microsecond = italian_time.microsecond
 
-    return utc_time
+    # Create a datetime with Italian time values
+    naive_italian = datetime(year, month, day, hour, minute, second, microsecond)
+
+    # Mark it as UTC (even though the values are actually Italian time)
+    # This matches the TypeScript behavior
+    utc_marked = pytz.UTC.localize(naive_italian)
+
+    return utc_marked
